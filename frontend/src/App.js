67d340a7a6265980bab34a67324f1ba1f1ca5e81
szludora <szlucska.dora@gmail.com>
2024-02-03 13:33:13 +0100

    hiányzó újratöltés funkció

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,36 @@
 import "./App.css";
+import Konyvek from "./component/Konyvek";
 import DataService from "./model/DataService";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import { useEffect, useState } from "react";
+import Tablazatom from "./component/Tablazatom";
 const DS = new DataService();
 
 function App() {
   let vegpont = "/books";
   const [objLista, setObjLista] = useState([{}]);
+  const lista = ["Id", "Szerző", "Cím", "Leírás", "", ""];
 
   useEffect(() => {
     DS.getData(vegpont, setObjLista);
   }, []);
+
+  function torles(id) {
+    console.log("App.js");
+    DS.deleteData(vegpont, id);
+  }
   return (
     <div className="App">
       <header className="App-header"></header>
-      <p>{objLista[0].book_id}</p>
-      <p>{objLista[0].author}</p>
-      <p>{objLista[0].title}</p>
+      <Container>
+        <Row style={{ marginTop: "2em" }}>
+          <Konyvek konyvek={objLista} />
+        </Row>
+        <Row>
+          <Tablazatom adatok={objLista} lista={lista} torles={torles} />
+        </Row>
+      </Container>
     </div>
   );
 }
