0319f4be307599c29aa849c2e52ffb61fbe2cc97
szludora <szlucska.dora@gmail.com>
2024-02-03 10:44:58 +0100

    react hozzáadása bootstrappel, axiossal

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import DataService from "./model/DataService"
+const DS = new DataService();
+
+
+function App() {
+  let vegpont = "books"
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
